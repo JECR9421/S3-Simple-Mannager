3c248186494dddda08fd0945abb7d37015b48f14
JECR9421 <ch2jsr@hotmail.com>
2023-08-04 00:04:40 -0600

    Testing

---
+++ src/test.ts
@@ -0,0 +1,14 @@
+import { S3Service } from ".";
+const AWS_REGION = 'us-west-2';
+const ENDPOINT = 'https://sfo2.digitaloceanspaces.com';
+const bucket = 'fedocumentsstorage';
+const fileName = 'test-input.xml';
+const folder = 'CIA602/AGENCIA-TEST';
+const pathToDownload = 'C:/DSign/Temp';
+const test = async () => {
+    const s3Mannager = new S3Service(AWS_REGION, ENDPOINT);
+    const result = await s3Mannager.dowload(bucket, fileName, folder, pathToDownload);
+    console.log('result', result);
+}
+
+test();
